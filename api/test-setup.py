6004f7671c011d291268264416b4824ef9692107
Sean Sube <seansube@gmail.com>
2023-01-05 15:38:29 -0600

    fix(api): match model path from readme

--- api/test-setup.py
+++ api/test-setup.py
@@ -6,6 +6,6 @@ height = 512
 width = 512
 prompt = "an astronaut eating a hamburger"
 
-pipe = OnnxStableDiffusionPipeline.from_pretrained("../../stable_diffusion_onnx", provider="DmlExecutionProvider", safety_checker=None)
+pipe = OnnxStableDiffusionPipeline.from_pretrained("../models/stable-diffusion-onnx-v1-5", provider="DmlExecutionProvider", safety_checker=None)
 image = pipe(prompt, height, width, num_inference_steps=steps, guidance_scale=cfg).images[0]
 image.save("../test.png")
