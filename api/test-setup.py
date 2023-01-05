ccbce9abe8ccfa152643e58ec0d36b60a9b76db5
Sean Sube <seansube@gmail.com>
2023-01-05 10:05:23 -0600

    add requirements file and test script

--- api/test-setup.py
+++ api/test-setup.py
@@ -1,11 +1,11 @@
 from diffusers import OnnxStableDiffusionPipeline
 
-cfg=8
-steps=22
-height=512
-width=512
+cfg = 8
+steps = 22
+height = 512
+width = 512
 prompt = "an astronaut eating a hamburger"
 
 pipe = OnnxStableDiffusionPipeline.from_pretrained("../../stable_diffusion_onnx", provider="DmlExecutionProvider", safety_checker=None)
 image = pipe(prompt, height, width, num_inference_steps=steps, guidance_scale=cfg).images[0]
-image.save("test.png")
+image.save("../test.png")
