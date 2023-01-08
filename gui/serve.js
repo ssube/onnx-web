fc988e4b5bc4d29a3a71121538f8d362490c3212
Sean Sube <seansube@gmail.com>
2023-01-08 09:46:13 -0600

    fix(gui): bind dev server to localhost by default, open binding in containers

--- gui/serve.js
+++ gui/serve.js
@@ -5,7 +5,7 @@ import { join } from 'path';
 
 const { env } = process;
 
-const host = mustDefault(env.ONNX_WEB_DEV_HOST, '0.0.0.0');
+const host = mustDefault(env.ONNX_WEB_DEV_HOST, '127.0.0.1');
 const port = mustDefault(env.ONNX_WEB_DEV_PORT, '3000');
 const root = process.cwd();
 
