760b162a5578ff5c5a091e6281f55452b520d157
Sean Sube <seansube@gmail.com>
2023-01-08 15:26:43 -0600

    fix: bump package versions to 0.2.1

--- api/setup.py
+++ api/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="onnx_web",
-    version="0.2.0",
+    version="0.2.1",
     author="ssube",
     author_email="seansube@gmail.com",
     description="web UI for running ONNX models",
