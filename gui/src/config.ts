5127a445325df5b959d017c766a878bab5bdcf21
Sean Sube <seansube@gmail.com>
2023-01-13 14:39:07 -0600

    feat(gui): save source and mask images while changing tabs

--- gui/src/config.ts
+++ gui/src/config.ts
@@ -1,3 +1,5 @@
+import { Maybe } from '@apextoaster/js-utils';
+
 import { Img2ImgParams, STATUS_SUCCESS, Txt2ImgParams } from './api/client.js';
 
 export interface ConfigNumber {
@@ -12,16 +14,20 @@ export interface ConfigString {
   keys: Array<string>;
 }
 
-export type KeyFilter<T extends object> = {
-  [K in keyof T]: T[K] extends number ? K : T[K] extends string ? K : never;
+export type KeyFilter<T extends object, TValid = number | string> = {
+  [K in keyof T]: T[K] extends TValid ? K : never;
 }[keyof T];
+
+export type ConfigFiles<T extends object> = {
+  [K in KeyFilter<T, Blob | File>]: Maybe<T[K]>;
+};
 
 export type ConfigRanges<T extends object> = {
   [K in KeyFilter<T>]: T[K] extends number ? ConfigNumber : T[K] extends string ? ConfigString : never;
 };
 
-export type ConfigState<T extends object> = {
-  [K in KeyFilter<T>]: T[K] extends number ? number : T[K] extends string ? string : never;
+export type ConfigState<T extends object, TValid = number | string> = {
+  [K in KeyFilter<T, TValid>]: T[K] extends TValid ? T[K] : never;
 };
 
 export type ConfigParams = ConfigRanges<Required<Img2ImgParams & Txt2ImgParams>>;
@@ -45,6 +51,7 @@ export const DEFAULT_BRUSH = {
 export const IMAGE_FILTER = '.bmp, .jpg, .jpeg, .png';
 export const STALE_TIME = 300_000; // 5 minutes
 export const POLL_TIME = 5_000; // 5 seconds
+export const SAVE_TIME = 5_000; // 5 seconds
 
 export async function loadConfig(): Promise<Config> {
   const configPath = new URL('./config.json', window.origin);
