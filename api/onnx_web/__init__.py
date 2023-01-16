e43238b327ce04d8895da134df63d85ab4c95c3a
Sean Sube <seansube@gmail.com>
2023-01-15 19:34:51 -0600

    fix init imports

--- api/onnx_web/__init__.py
+++ api/onnx_web/__init__.py
@@ -1,1 +1,10 @@
-from onnx_web.serve import get_and_clamp_float, get_and_clamp_int, get_from_map, get_latents_from_seed, load_pipeline
+from .pipeline import (
+  run_img2img_pipeline,
+  run_inpaint_pipeline,
+  run_txt2img_pipeline,
+)
+from .utils import (
+  get_and_clamp_float,
+  get_and_clamp_int,
+  get_from_map,
+)
