455bfddbc112fcc24b2f7dafee70912daebee511
Sean Sube <seansube@gmail.com>
2023-01-21 22:35:26 -0600

    fix(build): share layers from main image in feature branches

--- common/scripts/image-build.sh
+++ common/scripts/image-build.sh
@@ -6,14 +6,16 @@ IMAGE_NAME="${CI_PROJECT_PATH}-${IMAGE_SUFFIX}"
 IMAGE_TAG="$(echo "${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}" | sed -r 's/[^-_a-zA-Z0-9\\.]/-/g')-${IMAGE_ARCH}"
 
 IMAGE_FULL="${IMAGE_NAME}:${IMAGE_TAG}"
+IMAGE_MAIN="${IMAGE_NAME}:main-${IMAGE_ARCH}"
 
 echo "Loading last image: ${IMAGE_FULL}"
 
-docker pull "${IMAGE_FULL}" || echo "Failed to load last image."
+docker pull "${IMAGE_FULL}" || docker pull "${IMAGE_MAIN}" || echo "Failed to load last image."
 
 echo "Building image: ${IMAGE_FULL}"
 
 docker build \
+  --cache-from "${IMAGE_MAIN}" \
   --cache-from "${IMAGE_FULL}" \
   -f "${IMAGE_FILE}" \
   -t "${IMAGE_FULL}" . || { echo "Failed to build image!"; exit 1; }
