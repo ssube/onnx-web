e40fba80fbba4965fc1f8fc1779e17c1a55524ef
Sean Sube <seansube@gmail.com>
2023-01-21 23:00:11 -0600

    add script to copy bundle

---
+++ copy-bundle.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+VERSION="${1:-v0.5.0}"
+
+echo "Copying bundle for ${VERSION}..."
+
+mkdir -p ${VERSION}/bundle
+
+cp -v ../onnx-web/gui/out/index.html ${VERSION}/index.html
+cp -v ../onnx-web/gui/out/config.json ${VERSION}/config.json
+cp -v ../onnx-web/gui/out/bundle/main.js ${VERSION}/bundle/main.js
+
