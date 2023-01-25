d1ed5c48e8a733d4ffaad47b65c001a09b47b072
Sean Sube <seansube@gmail.com>
2023-01-25 17:06:37 -0600

    fix(build): sh-friendly syntax

--- common/scripts/make-venv.sh
+++ common/scripts/make-venv.sh
@@ -3,7 +3,7 @@
 if [ -d onnx_env ];
 then
   echo "Activating existing venv..."
-  source onnx_env/bin/activate
+  . onnx_env/bin/activate
 else
   echo "Creating new venv..."
   python3 -m venv onnx_env
