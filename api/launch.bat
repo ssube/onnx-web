fa7e5e8ab97769b9dbeef48618d75f8b8bb987a2
Sean Sube <seansube@gmail.com>
2023-01-31 22:53:41 -0600

    feat(api): add launch script for Windows

---
+++ api/launch.bat
@@ -0,0 +1,5 @@
+echo "Downloading and converting models to ONNX format..."
+python3 -m onnx_web.convert --diffusion --upscaling --correction --extras --token=%HF_TOKEN%
+
+echo "Launching API server..."
+flask --app=onnx_web.serve run --host=0.0.0.0
