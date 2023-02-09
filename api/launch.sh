85e6e7242a2e97021166f0d0d84cf3507417c74c
Sean Sube <seansube@gmail.com>
2023-02-08 22:41:54 -0600

    load local extra by default

--- api/launch.sh
+++ api/launch.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "Downloading and converting models to ONNX format..."
-python3 -m onnx_web.convert --diffusion --upscaling --correction --extras --token=${HF_TOKEN:-}
+python3 -m onnx_web.convert --diffusion --upscaling --correction --extras=./extras.json --token=${HF_TOKEN:-}
 
 echo "Launching API server..."
 flask --app=onnx_web.serve run --host=0.0.0.0
