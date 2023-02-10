d8d5bcd92739224a21afc4b1891b6f5d9ac8e22b
Sean Sube <seansube@gmail.com>
2023-02-09 22:02:27 -0600

    fix(api): make venv has been loaded in launch script

--- api/launch.sh
+++ api/launch.sh
@@ -2,8 +2,21 @@
 
 set -eu
 
+if [ -n "${VIRTUAL_ENV+set}" ]; then
+  echo "Using current virtual env..."
+else
+  if [ -d "onnx_env" ]; then
+    echo "Loading existing virtual env..."
+    . onnx_env/bin/activate
+  else
+    echo "Creating new virtual env..."
+    python -m venv onnx_env
+    . onnx_env/bin/activate
+  fi
+fi
+
 echo "Downloading and converting models to ONNX format..."
-python3 -m onnx_web.convert --diffusion --upscaling --correction --extras=./extras.json --token=${HF_TOKEN:-}
+python3 -m onnx_web.convert --diffusion --upscaling --correction --extras=extras.json --token=${HF_TOKEN:-}
 
 echo "Launching API server..."
 flask --app=onnx_web.serve run --host=0.0.0.0
