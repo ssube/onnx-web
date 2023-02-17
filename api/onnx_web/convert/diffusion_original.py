56a826228d93f6919c67806437f870e6010dec6a
Sean Sube <seansube@gmail.com>
2023-02-17 08:34:28 -0600

    fix(api): only remove converted VAE if it exists

--- api/onnx_web/convert/diffusion_original.py
+++ api/onnx_web/convert/diffusion_original.py
@@ -1428,7 +1428,8 @@ def convert_diffusion_original(
         logger.info("converted original Diffusers checkpoint to Torch model")
 
     # VAE has already been converted and will confuse HF repo lookup
-    del model["vae"]
+    if "vae" in model:
+        del model["vae"]
 
     convert_diffusion_stable(ctx, model, working_name)
     logger.info("ONNX pipeline saved to %s", name)
