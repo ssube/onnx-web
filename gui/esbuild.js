b1544a32e592cd1e75e458c77d0ae63ceff67447
Ben Fortune <benjamin.fortune1@gmail.com>
2023-02-17 21:46:56 +0000

    feat(build): add livereloading to esbuild
    remove unnecessary check
    
    lint esbuild

--- gui/esbuild.js
+++ gui/esbuild.js
@@ -1,8 +1,10 @@
-import { build } from 'esbuild';
+import { build, context } from 'esbuild';
 import { join } from 'path';
 import alias from 'esbuild-plugin-alias';
+import { copy } from 'esbuild-plugin-copy';
 
-const debug = process.env['DEBUG'] === 'TRUE'
+const debug = process.env['DEBUG'] === 'TRUE';
+const watch = process.env['WATCH'] === 'TRUE';
 const root = process.cwd();
 
 const plugins = [];
@@ -14,7 +16,7 @@ if (debug) {
     }));
 }
 
-build({
+const config = {
     bundle: true,
     define: {
         global: 'window',
@@ -27,4 +29,40 @@ build({
     platform: 'browser',
     plugins,
     sourcemap: true,
-}).catch(() => process.exit(1));
+};
+
+if(watch) {
+    const copyArray = (files) => files.map(file => 
+        copy({
+            resolveFrom: 'cwd',
+            assets: {
+                from: [file],
+                to: ['out/'],
+            },
+        })
+    );
+
+    const ctx = await context({
+        ...config,
+        entryPoints: [
+            join(root, 'src/main.tsx'),
+        ],
+        plugins: [
+            ...plugins,
+            ...copyArray(['src/index.html', 'examples/config.json']),
+        ],
+        banner: {
+            js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`,
+        },
+    });
+
+    await ctx.watch();
+
+    const { host, port } = await ctx.serve({
+        servedir: 'out/',
+    });
+
+    console.log(`Serving on http://${host}:${port}`);
+} else {
+    build(config).catch(() => process.exit(1));
+}
