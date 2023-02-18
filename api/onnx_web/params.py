f051af359d1f642ebe3a4a2a9357efaa065f9972
Sean Sube <seansube@gmail.com>
2023-02-18 11:05:23 -0600

    make default order match type

--- api/onnx_web/params.py
+++ api/onnx_web/params.py
@@ -174,7 +174,7 @@ class UpscaleParams:
         scale: int = 4,
         pre_pad: int = 0,
         tile_pad: int = 10,
-        upscale_order: Literal["correction-first", "correction-last", "correction-both"] = "upscaling-first",
+        upscale_order: Literal["correction-first", "correction-last", "correction-both"] = "correction-first",
     ) -> None:
         self.upscale_model = upscale_model
         self.correction_model = correction_model
