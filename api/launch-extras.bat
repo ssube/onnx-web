61373d530a4c1d21d46eb0194460f64482b73796
Sean Sube <seansube@gmail.com>
2023-02-27 20:08:52 -0600

    fix Windows entrypoint

--- api/launch-extras.bat
+++ api/launch-extras.bat
@@ -3,4 +3,4 @@ IF "%ONNX_WEB_EXTRA_MODELS%"=="" (set ONNX_WEB_EXTRA_MODELS=extras.json)
 python -m onnx_web.convert --sources --diffusion --upscaling --correction --extras=%ONNX_WEB_EXTRA_MODELS% --token=%HF_TOKEN%
 
 echo "Launching API server..."
-flask --app="onnx_web.serve:run" run --host=0.0.0.0
+flask --app="onnx_web.main:run" run --host=0.0.0.0
