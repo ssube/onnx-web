4fa9af792297f240fe71cbe08edbf7cfdc4852c7
Sean Sube <seansube@gmail.com>
2023-04-15 09:02:17 -0500

    hide unused pipeline options

--- api/onnx_web/diffusers/load.py
+++ api/onnx_web/diffusers/load.py
@@ -59,10 +59,10 @@ latent_factor = 8
 available_pipelines = {
     "controlnet": OnnxStableDiffusionControlNetPipeline,
     "img2img": OnnxStableDiffusionImg2ImgPipeline,
-    "inpaint": OnnxStableDiffusionInpaintPipeline,
+    #"inpaint": OnnxStableDiffusionInpaintPipeline,
     "lpw": OnnxStableDiffusionLongPromptWeightingPipeline,
     "pix2pix": OnnxStableDiffusionInstructPix2PixPipeline,
-    "txt2img": OnnxStableDiffusionPipeline,
+    #"txt2img": OnnxStableDiffusionPipeline,
 }
 
 pipeline_schedulers = {
