e89f2fc4d005f566bd2088b9571c441632b3efad
Sean Sube <seansube@gmail.com>
2023-04-21 22:45:44 -0500

    fix(exe): set default model cache to 0 for Windows bundle

--- exe/onnx-web-half.bat
+++ exe/onnx-web-half.bat
@@ -1,10 +1,14 @@
+REM paths required to make the bundle work
 set ONNX_WEB_BASE_PATH=%~dp0
 set ONNX_WEB_BUNDLE_PATH=%ONNX_WEB_BASE_PATH%\client
 set ONNX_WEB_MODEL_PATH=%ONNX_WEB_BASE_PATH%\models
-set ONNX_WEB_EXTRA_MODELS=%ONNX_WEB_BASE_PATH%\models\onnx-web-extras.json
 set ONNX_WEB_OUTPUT_PATH=%ONNX_WEB_BASE_PATH%\outputs
 
+REM customize these as needed
 set ONNX_WEB_BLOCK_PLATFORMS=cpu
+set ONNX_WEB_CACHE_MODELS=0
+set ONNX_WEB_EXTRA_MODELS=%ONNX_WEB_BASE_PATH%\models\onnx-web-extras.json
 
-@echo Launching onnx-web in half-precision mode...
+REM convert models and launch the server
+@echo Launching onnx-web in fp16 mode...
 server\onnx-web.exe --diffusion --correction --upscaling --half
