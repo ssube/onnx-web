570b56ab7252c90874436c129f478dcc25feaadd
Sean Sube <seansube@gmail.com>
2023-07-20 06:56:58 -0500

    fix list of file types

--- gui/src/components/Profiles.tsx
+++ gui/src/components/Profiles.tsx
@@ -80,7 +80,7 @@ export function Profiles(props: ProfilesProps) {
             <ImageSearch />
             <input
               hidden
-              accept={'*.json,*.jpg,*.jpeg,*.png,*.txt'}
+              accept={'.json,.jpg,.jpeg,.png,.txt,.webp'}
               type='file'
               onChange={(event) => {
                 const { files } = event.target;
