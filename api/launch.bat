2a641b111ec827477bf811134246163979fb6d7e
Sean Sube <seansube@gmail.com>
2023-12-10 17:03:54 -0600

    fix(api): check for web UI files in Windows launch scripts

--- api/launch.bat
+++ api/launch.bat
@@ -1,5 +1,7 @@
 call onnx_env\Scripts\Activate.bat
 
+echo "This launch.bat script is deprecated in favor of launch.ps1 and will be removed in a future release."
+
 echo "Downloading and converting models to ONNX format..."
 IF "%ONNX_WEB_EXTRA_MODELS%"=="" (set ONNX_WEB_EXTRA_MODELS=..\models\extras.json)
 python -m onnx_web.convert ^
@@ -9,6 +11,12 @@ python -m onnx_web.convert ^
 --correction ^
 --extras=%ONNX_WEB_EXTRA_MODELS% ^
 --token=%HF_TOKEN% %ONNX_WEB_EXTRA_ARGS%
+
+IF NOT EXIST .\gui\index.html (
+  echo "Please make sure you have downloaded the web UI files from https://github.com/ssube/onnx-web/tree/gh-pages"
+  echo "See https://github.com/ssube/onnx-web/blob/main/docs/setup-guide.md#download-the-web-ui-bundle for more information"
+  pause
+)
 
 echo "Launching API server..."
 waitress-serve ^
