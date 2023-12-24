75ac764d428e51be4726164d682a285c1160dbdc
Sean Sube <seansube@gmail.com>
2023-12-23 21:36:52 -0600

    feat: split up pre-converted models into full and base sets

--- exe/onnx-web-half.bat
+++ exe/onnx-web-half.bat
@@ -7,7 +7,7 @@ set ONNX_WEB_OUTPUT_PATH=%ONNX_WEB_BASE_PATH%\outputs
 REM customize these as needed
 set ONNX_WEB_BLOCK_PLATFORMS=cpu
 set ONNX_WEB_CACHE_MODELS=0
-set ONNX_WEB_EXTRA_MODELS=%ONNX_WEB_MODEL_PATH%\preconverted-fp16.json,%ONNX_WEB_MODEL_PATH%\extras.json
+set ONNX_WEB_EXTRA_MODELS=%ONNX_WEB_MODEL_PATH%\preconverted-base-fp16.json,%ONNX_WEB_MODEL_PATH%\extras.json
 
 REM convert models and launch the server
 @echo Launching onnx-web in fp16 mode...
